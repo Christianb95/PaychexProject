81916b48eaa6449833a79715a5c38a2e7a956906
ldesantis144 <ldesantis144@gmail.com>
2023-09-22 13:28:55 -0400

    Beginning of toJSON code

---
+++ toJSON.java
@@ -0,0 +1,9 @@
+/*TODO: store SQL results, convert to format to nest easily?, convert to JSON
+ */
+public class toJSON {
+    public static void main(String[] args){
+        /*
+         *
+         */
+    }
+}
