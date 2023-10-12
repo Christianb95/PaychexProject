c65ad38798c77669689a62ce888c92a9ac49b502
Christianb95 <60890512+Christianb95@users.noreply.github.com>
2023-10-12 12:44:10 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        try {
+            // Simulate a SQL to JSON conversion process
+            // If an error occurs, you can throw the custom error like this:
+            throw new SqlToJsonConversionError("Error converting SQL to JSON: Invalid SQL syntax.");
+
+
+        } catch (SqlToJsonConversionError e) {
+            System.err.println("SQL to JSON conversion error: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
